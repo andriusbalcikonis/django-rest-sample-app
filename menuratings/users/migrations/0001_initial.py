f8a2184fba0d24c0aa7c7bc047592755318a2f26
Andrius Balcikonis <andrius@balcikonis.com>
2020-06-21 12:24:21 +0300

    Complexity checks + some unignoring of flake8

--- menuratings/users/migrations/0001_initial.py
+++ menuratings/users/migrations/0001_initial.py
@@ -32,7 +32,7 @@ class Migration(migrations.Migration):
                     "is_superuser",
                     models.BooleanField(
                         default=False,
-                        help_text="Designates that this user has all permissions without explicitly assigning them.",
+                        help_text="Designates that this user has all permissions without explicitly assigning them.",  # noqa
                         verbose_name="superuser status",
                     ),
                 ),
@@ -81,7 +81,7 @@ class Migration(migrations.Migration):
                     "is_active",
                     models.BooleanField(
                         default=True,
-                        help_text="Designates whether this user should be treated as active. Unselect this instead of deleting accounts.",
+                        help_text="Designates whether this user should be treated as active. Unselect this instead of deleting accounts.",  # noqa
                         verbose_name="active",
                     ),
                 ),
@@ -104,7 +104,7 @@ class Migration(migrations.Migration):
                     "groups",
                     models.ManyToManyField(
                         blank=True,
-                        help_text="The groups this user belongs to. A user will get all permissions granted to each of their groups.",
+                        help_text="The groups this user belongs to. A user will get all permissions granted to each of their groups.",  # noqa
                         related_name="user_set",
                         related_query_name="user",
                         to="auth.Group",
