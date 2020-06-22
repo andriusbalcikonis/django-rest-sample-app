5d762425fca5aabc5738327f303cc45b5a217c7a
Andrius Balcikonis <andrius@balcikonis.com>
2020-06-22 21:08:27 +0300

    Debugging in pytest mode, trivial test

--- menuratings/users/test/test_app_logic.py
+++ menuratings/users/test/test_app_logic.py
@@ -1,6 +1,9 @@
 import pytest
 from menuratings.menuratings.models import Restaurant
 
+from django.urls import reverse
+from rest_framework import status
+
 
 @pytest.mark.django_db
 def test_smth():
@@ -14,3 +17,11 @@ def test_smth():
     all = Restaurant.objects.all()
 
     assert len(all) == 2
+
+
+@pytest.mark.django_db
+def test_user_list(client):
+    url = reverse("user-list")
+    response = client.get(url)
+    assert response.status_code == status.HTTP_200_OK
+    assert len(response.data["results"]) == 0
