59e340679f7de1a99df7156ce94237d3b09316d5
Andrius Balcikonis <andrius@balcikonis.com>
2020-06-23 11:06:11 +0300

    Create admin user in tests

--- menuratings/mr/test/test_app_logic.py
+++ menuratings/mr/test/test_app_logic.py
@@ -1,27 +1,17 @@
 import pytest
-from menuratings.mr.models import Restaurant
+from menuratings.mr.models import User
 
 from django.urls import reverse
 from rest_framework import status
 
 
 @pytest.mark.django_db
-def test_smth():
-
-    r = Restaurant(name="From tests")
-    r.save()
-
-    r = Restaurant(name="From tests 2")
-    r.save()
-
-    all = Restaurant.objects.all()
-
-    assert len(all) == 2
-
-
-@pytest.mark.django_db
 def test_user_list(client):
-    url = reverse("user-list")
-    response = client.get(url)
+
+    password = "admin"
+    admin = User.objects.create_superuser("admin", "admin@admin.com", password)
+    client.login(username=admin.username, password=password)
+
+    response = client.get(reverse("user-list"))
     assert response.status_code == status.HTTP_200_OK
-    assert len(response.data["results"]) == 0
+    assert len(response.data["results"]) == 1
