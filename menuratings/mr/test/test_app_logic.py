095213d74db75077c334b82eefbe41ab041c9239
Andrius Balcikonis <andrius@balcikonis.com>
2020-06-23 11:39:00 +0300

    First tests for user list

--- menuratings/mr/test/test_app_logic.py
+++ menuratings/mr/test/test_app_logic.py
@@ -1,17 +1,58 @@
-import pytest
 from menuratings.mr.models import User
 
 from django.urls import reverse
 from rest_framework import status
 
+ADMIN_USER_USERNAME = "admin"
+ADMIN_USER_PWD = "admin"
 
-@pytest.mark.django_db
-def test_user_list(client):
 
-    password = "admin"
-    admin = User.objects.create_superuser("admin", "admin@admin.com", password)
-    client.login(username=admin.username, password=password)
+# Tests - user list
 
-    response = client.get(reverse("user-list"))
+
+def test_user_list_for_anonymous_user_is_accessible_but_empty(client, db):
+    set_initial_conditions_empty_db()
+    response = http_get_user_list(client)
+    assert_result_count(response, 0)
+
+
+def test_user_list_for_admin_user_is_accessible_and_has_one_item(client, db):
+    set_initial_conditions_only_admin_user()
+    login_as_admin(client)
+    response = http_get_user_list(client)
+    assert_result_count(response, 1)
+
+
+# Helpers - initial conditions:
+
+
+def set_initial_conditions_empty_db():
+    pass
+
+
+def set_initial_conditions_only_admin_user():
+    User.objects.create_superuser(
+        ADMIN_USER_USERNAME, "admin@admin.com", ADMIN_USER_PWD
+    )
+
+
+# Helpers - login:
+
+
+def login_as_admin(client):
+    client.login(username=ADMIN_USER_USERNAME, password=ADMIN_USER_PWD)
+
+
+# Helpers - http requests:
+
+
+def http_get_user_list(client):
+    return client.get(reverse("user-list"))
+
+
+# Helpers - assertions
+
+
+def assert_result_count(response, count):
     assert response.status_code == status.HTTP_200_OK
-    assert len(response.data["results"]) == 1
+    assert len(response.data["results"]) == count
