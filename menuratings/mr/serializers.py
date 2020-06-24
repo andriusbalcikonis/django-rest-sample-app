b20814968acdd1d045f20ccb14619fae51e45a73
Andrius Balcikonis <andrius@balcikonis.com>
2020-06-24 17:11:21 +0300

    Stats calculation and showing next to options

--- menuratings/mr/serializers.py
+++ menuratings/mr/serializers.py
@@ -6,6 +6,7 @@ from menuratings.mr.models import (
     Vote,
     User,
 )
+from menuratings.mr.external import get_todays_date
 
 
 class CreateUserSerializer(serializers.ModelSerializer):
@@ -104,9 +105,40 @@ class MyTodaysOptionsMenuSerializer(serializers.HyperlinkedModelSerializer):
         many=False, read_only=True, slug_field="name"
     )
 
+    votes_in_my_organization = serializers.SerializerMethodField(
+        "get_votes_in_my_organization"
+    )
+
+    def get_votes_in_my_organization(self, current_menu_item):
+        """
+        Calculates todays votes for this menu option and only in the context of users org.
+        """
+
+        current_user = self.get_context_user()
+
+        if current_user:
+            votes = (
+                Vote.objects.filter(menu_id=current_menu_item.id)
+                .filter(
+                    voter__represented_organization_id=current_user.represented_organization.id
+                )
+                .count()
+            )
+
+            return votes
+        else:
+            return -1
+
+    def get_context_user(self):
+        request = self.context.get("request")
+        if request and hasattr(request, "user"):
+            return request.user
+        else:
+            return None
+
     class Meta:
         model = Menu
-        fields = ["restaurant", "contents"]
+        fields = ["restaurant", "contents", "votes_in_my_organization"]
 
 
 class MyVoteSerializer(serializers.HyperlinkedModelSerializer):
@@ -123,7 +155,8 @@ class MyVoteSerializer(serializers.HyperlinkedModelSerializer):
 class MyVoteSerializerForEditing(serializers.HyperlinkedModelSerializer):
     class MenuOptionsField(serializers.PrimaryKeyRelatedField):
         def get_queryset(self):
-            return Menu.objects.all()
+            today = get_todays_date()
+            return Menu.objects.filter(date=today)
 
     menu = MenuOptionsField(many=False, read_only=False)
 
