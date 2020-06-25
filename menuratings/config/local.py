5bdefbbbee44c60593a5e4a12ab7eed8d30698dd
Andrius Balcikonis <andrius@balcikonis.com>
2020-06-25 16:50:32 +0300

    Minio docker & django setup

--- menuratings/config/local.py
+++ menuratings/config/local.py
@@ -15,3 +15,10 @@ class Local(Common):
             conn_max_age=int(os.getenv("POSTGRES_CONN_MAX_AGE", 600)),
         )
     }
+
+    MINIO_SERVER = "localhost"
+    MINIO_ACCESSKEY = "Q3AM3UQ867SPQQA43P2F"
+    MINIO_SECRET = "zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG"
+    MINIO_BUCKET = "menuratings_bucket"
+    MINIO_SECURE = True
+    DEFAULT_FILE_STORAGE = "django_minio.storage.MinioStorage"
